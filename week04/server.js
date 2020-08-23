e289c34fbd270b4eb9da46acf77d276dc279988a
lyb <975965948@qq.com>
2020-08-23 22:46:25 +0800

    计算排版

---
+++ week04/server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    body.push(Buffer.from(chunk));
+    // body.push(chunk.toString());
+  }).on('end', () => {
+    body = Buffer.concat(body);
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(
+`<html maaa=a >
+  <head>
+    <style>
+      #container {
+        width: 500px;
+        height:300px;
+        display:flex;
+      }
+      #container #myid {
+        width: 200px;
+      }
+      #container .c1{
+        flex: 1;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="container">
+      <div id="myid"/>
+      <div class="c1"/>
+    </div>
+  </body>
+</html>`);
+  });
+}).listen(8088);
+console.log("server started");
